647a77493c8680919967160db2b9aa897fc0bc80
Joseph Sabido <joesabido@gmail.com>
2018-09-15 06:49:32 -0700

    socket testing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Axios from 'axios'
+import { TextField, Button, CssBaseline } from '@material-ui/core';
+
+const basepath = 'http://localhost:3000/api'
 
 class Main extends React.Component{
+	handleEnableMotors = async () => {
+		await Axios.put(`${basepath}/motors/enabled`)
+	}
+
 	render(){
 		return(
 			<React.Fragment>
-				
+				<CssBaseline />
+				<TextField helperText='Incoming!' multiline={true} rows={10} style={{width:'400px'}} value={this.props.log.join('\n')}/>
+				<Button variant='contained' onClick={this.handleEnableMotors}>Enable Motors</Button>
 			</React.Fragment>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import SocketIOClient from 'socket.io-client'
 import { CssBaseline } from '@material-ui/core';
 
 import Main from './components/Main'
 
 class App extends React.Component{
+	constructor(){
+		super()
+		this.socket = SocketIOClient('http://localhost:3000')
+		this.state = {
+			log : []
+		}
+	}
+
+	componentDidMount = () => {
+		this.socket.on('data', data => {
+			this.setState({
+				log : this.state.log.concat(data.replace(/\s+$/, ''))
+			})
+		})
+	}
+
 	render(){
 		return(
 			<React.Fragment>
 				<CssBaseline />
-				<Main />
+				<Main log={this.state.log}/>
 			</React.Fragment>
 		)
 	}
